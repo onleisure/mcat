ab3add2ab492d6b2f80bf2d17ab267916eab18a0
zhaoxy <zzzzhaoxy@163.com>
2018-11-23 11:19:09 +0800

    mcat2.0

---
+++ mcat-management/src/main/java/www/mcat/com/mcatmanagement/service/impl/ManageService.java
@@ -0,0 +1,11 @@
+package www.mcat.com.mcatmanagement.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManageService {
+
+    public void test(){
+        System.out.println("are you ok?");
+    }
+}

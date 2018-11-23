9263fcec08474e2ccee8a33f672b2e4a789c9cb0
zhaoxy <zzzzhaoxy@163.com>
2018-11-23 10:50:30 +0800

    mcat1.0

---
+++ mcat-list-service/src/main/java/www/mcat/com/list/McatListServiceApplication.java
@@ -0,0 +1,12 @@
+package www.mcat.com.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McatListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McatListServiceApplication.class, args);
+	}
+}
---
+++ mcat-management/src/main/java/www/mcat/com/mcatmanagement/McatManagementApplication.java
@@ -0,0 +1,12 @@
+package www.mcat.com.mcatmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McatManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McatManagementApplication.class, args);
+	}
+}
